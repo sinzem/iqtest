6b59d8ee5814fd0a32d838f3d4253b3265f6291d
Sergey <sinzem@gmail.com>
2024-07-20 21:22:26 +0300

    menu_first-page

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // for menu
+    const cross = document.querySelector(".cross");
+    const menu = document.querySelector(".menu");
+    const hamburger = document.querySelector(".hamburger");
+
+    cross.addEventListener("click", () => {
+        if (menu.classList.contains("active")) {
+            menu.classList.remove("active");
+            document.body.style.overflow = ""; 
+        }
+    })
+
+    hamburger.addEventListener("click", () => {
+        if (!menu.classList.contains("active")) {
+            menu.classList.add("active");
+            document.body.style.overflow = "hidden"; 
+        }
+    })
+})
